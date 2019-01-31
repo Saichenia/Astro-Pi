aaf33e99badb7daf7e7d80c3874d6cef8d5c7440
Tomás Alvim <thomasalvim@gmail.com>
2019-01-31 17:04:29 +0000

    queimada Flame.py
    Adiciona uma arvore queimada

--- Flame.py
+++ Flame.py
@@ -5,10 +5,10 @@ e = [250, 205, 5]
 l = [250, 130, 10]
 c = [250, 80, 50]
 a = [250,225,13]
+c1 =[67,39,39] #castanho escuro
+c2 = [51,51,51] #cinzento 
 
-
-
-[
+img1 = [
     o,o,o,o,r,o,o,o,
     o,o,o,o,r,r,o,o,
     o,o,o,r,c,c,o,o,
@@ -20,3 +20,15 @@ a = [250,225,13]
 
     ]
     
+    #And another one:
+    
+img2 = [
+     o,o,o,o,o,o,o,o,
+    o,o,o,o,o,c2,c2,o,
+    o,o,o,c2,c2,c2,c2,c2,
+    o,o,o,c2,c2,c2,c2,c2,
+    o,o,o,c2,c2,c1,c1,o,
+    o,o,o,o,o,c1,c1,o,
+    o,o,o,o,o,c1,c1,o,
+    o,o,o,o,c1,c1,o,o,
+    ]
