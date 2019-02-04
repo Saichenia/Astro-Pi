1315a2b9cb88d5aa8684432429477eecb5a455cb
Tomás Alvim <thomasalvim@gmail.com>
2019-02-04 17:42:05 +0000

    Add files via upload

--- AstroPi.py
+++ AstroPi.py
@@ -27,7 +27,7 @@ r = [73,40,21]
 e = [73, 70, 2]
 l = [73, 51, 5]
 c = [73, 40, 23]
-a = [73,100,50]
+a = [80, 80, 50]
 g = [80, 56, 39]
 b = [24, 64, 64]
 
@@ -54,12 +54,12 @@ img2 = [
     o,o,o,o,o,o,g,g,
     ]
 
-
-
 longrad = 0
 latrad = 0
+
 global photo_counter
-photo_counter = 0
+photo_counter = 1
+
 checker = 0
 cpu = CPUTemperature()
        
@@ -71,8 +71,6 @@ logzero.logfile(dir_path+"/data01.csv")
 formatter = logging.Formatter('%(name)s - %(asctime)-15s - %(levelname)s: \n%(message)s');
 logzero.formatter(formatter)
 
-
-n = 0.0 
 
 name = "ISS (ZARYA)"
 
@@ -89,21 +87,18 @@ iss = ephem.readtle(name, line1, line2)
 cam = PiCamera()
 cam.resolution = (2592,1944)
 iss.compute()
-print(iss.sublat, iss.sublong)
-n = iss.sublat
-print("%f" % (iss.sublat),"%f" %  n, ephem.degrees(n))
 
 def update_image(checker):
 
         # a list with all possible rotation values
     orientation = [0,90,270,180]
-    print(checker)
     if checker == 0:
     # pick one at random
         rotation = random.choice(orientation)
         # set the rotation
         sh.set_rotation(rotation)
     elif checker == 1:
+        sh.set_rotation(0)
         sh.flip_h()
 
 def get_latlon():
@@ -129,7 +124,6 @@ def get_latlon():
         cam.exif_tags['GPS.GPSLatitudeRef'] = "N"
 
     cam.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/10' % (lat_value[0], lat_value[1], lat_value[2]*10)
-    print(str(lat_value), str(long_value))
     return(str(lat_value), str(long_value))
 
 
@@ -140,10 +134,10 @@ def get_photo():
         # get latitude and longitude
         lat, lon = get_latlon()
         # Save the data to the file
-        logger.info("%s,%s,%s", "photo_"+ str(photo_counter).zfill(5), lat, lon )
+        logger.info("%s,%s,%s", "image_"+ str(photo_counter).zfill(5), lat, lon )
         # use zfill to pad the integer value used in filename to 3 digits (e.g. 001, 002...)
                     
-        cam.capture(dir_path+"/photo_"+ str(photo_counter).zfill(5)+".jpg")
+        cam.capture(dir_path+"/image_"+ str(photo_counter).zfill(5)+".jpg")
         photo_counter += 1
                     
     except Exception as e:
@@ -157,9 +151,8 @@ start_time2 = time.time()
 # (these will be almost the same at the start)
 now_time = datetime.datetime.now()
 # run a loop for 2 minutes
-#sh.set_pixels(img1)
-#cam.start_preview()
-#cam.start_preview()
+sh.set_pixels(img2)
+
 while ((now_time < start_time + datetime.timedelta(minutes=175)) and (photo_counter < 980) and (cpu.temperature < 70)):
     iss.compute()
     latrad = float("%f" % (iss.sublat))
@@ -185,9 +178,9 @@ while ((now_time < start_time + datetime.timedelta(minutes=175)) and (photo_coun
         (2.111045 < longrad < 2.399285 and -0.413486 < latrad < -0.195962),#Top Australia
         (1.997895 < longrad < 2.176838 and -0.490407 < latrad < -0.61162),# South left Australia
         (2.377186 < longrad < 2.688272 and -0.675403 < latrad < -0.421843)]#South right Australia
-    print("Americas: ", americas)
-    print("Central: ", central)
-    print("Oceasia: ", oceasia)
+    #print("Americas: ", americas)
+    #print("Central: ", central)
+    #print("Oceasia: ", oceasia)
     print(latrad, longrad)   
     if longrad < -0.442:
         if any(americas):
@@ -204,12 +197,11 @@ while ((now_time < start_time + datetime.timedelta(minutes=175)) and (photo_coun
     elif longrad > 1.058:
         if any(oceasia):
             get_photo()
+            sh.set_pixels(img1)
             checker = 1
             
     if checker == 0:
        sh.set_pixels(img2)
-    elif checker == 1:
-        sh.set_pixels(img1)
         
     sleep(2)
 
@@ -221,10 +213,8 @@ while ((now_time < start_time + datetime.timedelta(minutes=175)) and (photo_coun
     cpu = CPUTemperature()
     checker = 0
     print("Temp: ", cpu.temperature)
-    print("Time: ", time.time()-start_time2)
     
     now_time = datetime.datetime.now()
 
 cam.close()
 print("Finished. Signing out.")
-
